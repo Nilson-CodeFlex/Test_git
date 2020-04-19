b79fd2b5b3a4f22a6f45b24e7add483881cf691b
NilsonCode <nilsonvargas136@gmail.com>
2020-04-19 12:03:16 -0500

    Se añadio texto2.txt ,  hola.java y holaPython.py

---
+++ proyectoJava/hola.java
@@ -0,0 +1,9 @@
+	
+	public class hola{
+		public static void main (String[]args){
+
+		System.out.println("Hola mundo desde el cambio del repositorio");
+				
+
+	}
+     }	
---
+++ proyectoJava/holaPython.py
@@ -0,0 +1,1 @@
+print("Hola en mundo hecho en python")
